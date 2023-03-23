f3cf9326a19515a0c8bf27d47932f63006dd7fe9
amateur_dev <me@dipeshsukhani.dev>
2023-03-23 22:35:16 +0800

    updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,37 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: '0.8.5',
+    settings: {
+      optimizer: {
+        runs: 200,
+        enabled: true
+      }
+    }
+  },
+    networks: {
+      polygon: {
+        url: "https://rpc.ankr.com/polygon",
+        accounts:["INSERT PRIVATE KEY HERE"]
+      }
+    },
+    etherscan: {
+      apiKey: {
+        polygon: "INSERT POLYGONSCAN API KEY HERE",
+      },
+      customChains: [
+        {
+          network: "polygon",
+          chainId: 137,
+          urls: {
+            apiURL: "https://api.polygonscan.com/",
+            browserURL: "https://polygonscan.com/"
+          },
+        },
+      ],
+    }
 };
