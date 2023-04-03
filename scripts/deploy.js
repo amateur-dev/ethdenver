1b5550b029766144a8bcd9d17d145296e08daa09
Tcadle434 <thomascadle434@gmail.com>
2023-04-03 12:57:05 -0600

    splitting chainlink logic for lower callback gas

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,11 +9,7 @@ const hre = require('hardhat');
 async function main() {
   const Manager = await hre.ethers.getContractFactory('Manager');
   console.log('Starting the deployment...');
-  const manager = await Manager.deploy(
-    '0x3d2341ADb2D31f1c5530cDC622016af293177AE0',
-    '0xb0897686c545045aFc77CF20eC7A532E3120E0F1',
-    '0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da'
-  );
+  const manager = await Manager.deploy(701);
   console.log('deploying...');
   console.log(manager.deployTransaction);
 
@@ -22,11 +18,7 @@ async function main() {
 
   await hre.run('verify:verify', {
     address: manager.address,
-    constructorArguments: [
-      '0x3d2341ADb2D31f1c5530cDC622016af293177AE0',
-      '0xb0897686c545045aFc77CF20eC7A532E3120E0F1',
-      '0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da',
-    ],
+    constructorArguments: [701],
   });
 }
 
